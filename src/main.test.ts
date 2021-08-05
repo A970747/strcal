3e818ef05a6f6d2572836eca27f6d853a3ce3270
a970747 <Matt.J.Jackson@gmail.com>
2021-08-05 18:32:29 -0400

    Code before clean-up

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,7 +1,84 @@
-import { testerino } from './main';
+import StringCalc, { NegativeNumErr } from './main';
+import { longStringOfNums } from './longNum'
+const strcal = new StringCalc();
 
-describe('App', () => {
-  it('First test', () => {
-    expect(testerino()).toBe(0);
+
+describe('1st requirement', () => {
+  it('Empty string should return 0', () => {
+    expect(strcal.add("")).toBe(0);
+  })
+
+  it('Should handle a basic string of numbers', () => {
+    expect(strcal.add("1,2,3")).toBe(6);
+  })
+
+  it('Should handle a basic string of numbers with whitespace', () => {
+    expect(strcal.add("1, 2, 3,     4")).toBe(10);
+  })
+
+  it('Should use the decorator', () => {
+    for(let i = 0; i < 100; i++) {
+      expect(strcal.add(longStringOfNums)).toBe(50000);
+    }
+  })
+});
+
+describe('2nd requirement', () => {
+  it('Should handle new lines in input', () => {
+    expect(strcal.add("10\n,15, 25")).toBe(50);
+  });
+
+  it('Should handle a new line before a number in input', () => {
+    expect(strcal.add("\n10,15,25")).toBe(50);
+  });
+
+  it('Should handle a new line before a number in input anywhere in array', () => {
+    expect(strcal.add("10,15,25,50,\n100")).toBe(200);
+  });
+})
+
+describe('3rd requirement', () => {
+  it('Should handle a custom delimiter', () => {
+    expect(strcal.add("//;\n1;3;4")).toBe(8);
+  })
+
+  it('Should handle a custom delimiter that is same as default', () => {
+    expect(strcal.add("//,\n1,3,4")).toBe(8);
+  })
+
+  it('Should handle multiple delimiters', () => {
+    expect(strcal.add("//;,@,%,$\n1;3@4$5%6")).toBe(19);
+  })
+
+})
+
+describe('4th requirement', () => {
+  it('Should throw error with negative number', () => {
+    expect(strcal.add("//;\n1;-3;4")).toBe(NegativeNumErr);
+  })
+})
+
+describe('Bonus requirements', () => {
+  it('Should handle a custom delimiter', () => {
+    expect(strcal.add("//;\n1;1000;4")).toBe(5);
+  })
+
+  it('Should handle multiple delimiters', () => {
+    expect(strcal.add("//***\n1***2***3")).toBe(6);
+  })
+})
+
+describe('Additional coverage tests', () => {
+  it('Test getDelimiters has been called', () => {
+    const newStrCalc = new StringCalc();
+    const testDelimiterCheck = jest.spyOn(StringCalc.prototype as any, 'getDelimiters');
+    expect(newStrCalc.add("//,\n1,2,3,4")).toBe(10);
+    expect(testDelimiterCheck).toHaveBeenCalled();
+  })
+
+  it('Check branch for cached value if statement', () => {
+    expect(strcal.hasCachedValue("1,2,3,4,5,6")).toBe(false);
+    expect(strcal.add("1,2,3,4,5,6")).toBe(21);
+    expect(strcal.hasCachedValue("1,2,3,4,5,6")).toBe(true);
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,67 @@
-export function testerino() {
-  return 0;
+export const NegativeNumErr = Error("Negative numbers not permitted");
+export default class StringCalc {
+  private static cache = new Map();
+
+  public add(numberString: string): number | Error {
+    if(numberString === "") return 0;
+    if(StringCalc.cache.has(numberString)) return StringCalc.cache.get(numberString);
+
+
+    const delimiters = this.getDelimiters(numberString)
+
+/*     let delimiterRegEx = new RegExp(`[${delimiters}]`);
+    stringCopy = this.trimDelimiters(numberString).split(delimiterRegEx); */
+  
+    const numberArray: number[] = this.stringToNumArray(this.trimDelimiters(numberString), delimiters);
+
+    return this.calculateSum(numberArray, numberString);
+  }
+
+  public hasCachedValue(string:string): boolean {
+    return StringCalc.cache.has(string);
+  }
+
+  private getDelimiters(string: string): string[] {
+    const regex = new RegExp('\/\/(.*)\\n', 'g')
+    let delimiters: string[] = [","];
+    let matches = regex.exec(string)
+
+    if(matches) {
+      if(matches[1].length > 1) delimiters = matches[1].split(",");
+      if(matches[1].length == 1 && matches[1] !== ",") delimiters = [matches[1]]
+    }
+
+    return delimiters;
+  }
+
+  private stringToNumArray(numberString: string, delimiters: string[]): number[] {
+    numberString = numberString.replace(/ /g,"").replace(/(\n)/g,"");
+
+    delimiters.forEach( delim => {
+      numberString = numberString.replaceAll(delim," ");
+    })
+
+    return numberString.split(" ").map( num => parseInt(num));
+  }
+
+  private trimDelimiters(string: string): string {
+    const regex = new RegExp('\/\/(.*)\\n', 'g')
+    let stringNoDelimiters = string.replace(regex,"")
+
+    return stringNoDelimiters;
+  }
+
+  private calculateSum(numbers: number[], originalString: string): number | Error {
+    let negativeCount = 0;
+
+    const sumOfNumbers = numbers.reduce( (total, val) => {
+      if(val >= 1000) return total;
+      if(val < 0) negativeCount++;
+      return total + val
+    });
+    StringCalc.cache.set(originalString, sumOfNumbers);
+
+    if(negativeCount > 0) return NegativeNumErr;
+    return sumOfNumbers;
+  }
 }
